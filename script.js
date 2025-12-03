af6ff9a5cd1e8fcdcd7a161aa49916b2e7f0298b
Luiz Eduardo <ltibes925@gmail.com>
2025-12-03 16:28:00 -0300

    exercicio 6

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-const input = document.getElementById("nomeInput")
-    const addBtn = document.getElementById("addBtn")
-    const clearBtn = document.getElementById("clearBtn")
-    const lista = document.getElementById("lista")
-
-    addBtn.addEventListener("click", () => {
-      const nome = input.value.trim()
-      if (nome !== "") {
-        const li = document.createElement("li")
-        li.textContent = nome
-        lista.appendChild(li)
-        input.value = ""
-      }
-    })
-
-    clearBtn.addEventListener("click", () => {
-      lista.innerHTML = ""
+const botao = document.getElementById("darkBtn")
+    botao.addEventListener("click", () => {
+      document.body.classList.toggle("dark")
     })
